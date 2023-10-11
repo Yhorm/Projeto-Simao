01f70b6e0ed42f92d72cad3d8be208b4715bdb7a
Felipe da Silva Mossato <demongo90@hotmail.com>
2023-10-11 00:15:05 -0300

    COMEÇO DO PROJETO

---
+++ Projeto-Simao.cpp
@@ -0,0 +1,12 @@
+﻿// Projeto-Simao.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "Projeto-Simao.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "\a" << endl;
+	return 0;
+}
---
+++ Projeto-Simao.h
@@ -0,0 +1,8 @@
+﻿// Projeto-Simao.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
